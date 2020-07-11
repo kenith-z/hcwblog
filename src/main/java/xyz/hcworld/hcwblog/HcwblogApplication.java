0a913e41ca0a1c423cb60a34683c11d40f38a70d
hcw <zh@hcworld.xyz>
2020-07-11 15:16:34 +0800

    create porject hcwblog

---
+++ src/main/java/xyz/hcworld/hcwblog/HcwblogApplication.java
@@ -0,0 +1,13 @@
+package xyz.hcworld.hcwblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcwblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcwblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/hcworld/hcwblog/HcwblogApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.hcworld.hcwblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcwblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
